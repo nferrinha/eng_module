ff0212e6e808f0c0312ec9af089fd781117547d9
nferrinha <ferrinha.nuno@gmail.com>
2024-01-30 00:09:21 +0400

    initial commit

---
+++ beams.py
@@ -0,0 +1,90 @@
+import math
+from PyNite import FEModel3D
+
+def str_to_int(s: str) -> int:
+    """
+    Returns an integer if the string 's' represents an integer
+    """
+    return int(s)
+
+def str_to_float(s: str) -> float:
+    """
+    Returns an float if the string 's' represents an float
+    """
+    return float(s)
+
+def calc_shear_modulus (nu:float, E:float) -> float:
+    """
+    Return the shear moduls calculated from 'nu' and 'E'
+    """
+    G=E/(2*(1+nu))
+    return G
+
+def euler_buckling_load(
+    l: float,
+    E: float,
+    I: float,
+    k: float,
+) -> float:
+    """
+    Returns the Euler critical load for the column section described by 'l', 'E', 'I' and 'k'.
+    'l': braced length of the column
+    'E': Elastic moduls
+    'I': Second moment of area
+    'k': Effective length factor
+    """
+    P_cr=math.pi**2*E*I/(k*l)**2
+    return P_cr
+
+def beam_reactions_ss_cant(
+    w:float,
+    b:float,
+    a:float,
+) -> tuple[float,float]:
+    """
+    Returms the reactions "R1" and "R2" for a simply supported beam
+    with a continuous cantilever on one end. R2 is the backspan support and R1 is the hammer support.
+    """
+    length=b+a
+    centroid_of_udl=length/2
+    equiv_point_load=w*length
+    r1=equiv_point_load*centroid_of_udl/b
+    r2=equiv_point_load-r1
+    return -r1,-r2
+
+def fe_model_ss_cant(
+    w: float,
+    b: float,
+    a: float,
+    E: float, 
+    I: float, 
+    A: float, 
+    J: float, 
+    nu: float, 
+    rho: float=1.,
+)->FEModel3D:
+    """
+    Return a Pynite.FEModel3D model of a simply supported beam
+    with a cantilever on one end. The beam is loaded with a UDL
+
+    "w": The magnitude of the UDL
+    "b": The length of the backspan
+    "a": The length of the cantilever
+    "E": The elastic modulus
+    "I": The second moment of inertia
+    "A": The cross-sectional area
+    "J": The polar moment of inertia
+    "nu": Poisson's ratio of material
+    "rho": Density of the material
+    """
+    model=FEModel3D()
+    G=calc_shear_modulus(nu,E)
+    model.add_material('default',E,G,nu,rho)
+    model.add_node("N0",0,0,0)
+    model.add_node("N1",b,0,0)
+    model.add_node("N2",b+a,0,0)
+    model.def_support("N0",True,True,True,True,True,False)
+    model.def_support("N1",False,True,False,False,False,False)
+    model.add_member("M0","N0","N2",'default',Iy=1.0,Iz=I,J=J,A=A)
+    model.add_member_dist_load("M0","Fy",w1=w,w2=w)
+    return model
---
+++ test_beams.py
@@ -0,0 +1,99 @@
+import eng_module.beams as beams
+def test_str_to_int():
+    string_1 = "43"
+    string_2 = "2000"
+      
+    assert beams.str_to_int(string_1)==43
+    assert beams.str_to_int(string_2)==2000
+
+def test_str_to_float():
+    string_1 = "43"
+    string_2 = "2000"
+    string_3 = "324.625"
+    assert beams.str_to_float(string_1)==43.0
+    assert beams.str_to_float(string_2)==2000.0
+    assert beams.str_to_float(string_3)==324.625
+
+def test_calc_shear_modulus():
+    # Material 1
+    E1 = 200000 # MPa
+    nu1 = 0.3
+
+    # Material 2
+    E2 = 3645 # ksi ("ksi" == "kips per square inch"; "kips" == "kilopound" == 1000 lbs)
+    nu2 = 0.2   
+
+    assert beams.calc_shear_modulus(nu1,E1) == 76923.07692307692
+    assert beams.calc_shear_modulus(nu2,E2) == 1518.75
+
+def test_euler_buckling_load():
+    # Column 1 - Value will be in Newtons
+    l1 = 5300 # mm
+    E1 = 200000 # MPa
+    I1 = 632e6 # mm**4
+    k1 = 1.0
+
+    # Column 2 - Value will be in kips ("kips" == "kilopound" == 1000 lbs)
+    l2 = 212 # inch
+    E2 = 3645 # ksi ("ksi" == "kips per square inch")
+    I2 = 5125.4 # inch**4
+    k2 = 2.0
+
+    assert beams.euler_buckling_load(l1,E1,I1,k1)==44411463.02234584
+    assert beams.euler_buckling_load(l2,E2,I2,k2)==1025.6361727834453
+
+def test_beam_reactions_ss_cant():
+    w1 = 50 # kN/m (which is the same as N/mm)
+    a1 = 2350 # mm
+    b1 = 4500 # mm
+
+    w2 = 19 # lbs/inch == 228 lbs/ft
+    a2 = 96 # inch
+    b2 = 96 # inch
+
+    r1_1,r2_1=beams.beam_reactions_ss_cant(w1,b1,a1)
+    r1_2,r2_2=beams.beam_reactions_ss_cant(w2,b2,a2)
+   
+    assert (round(r1_1,2),round(r2_1,2))==(-260680.56,-81819.44)
+    assert (round(r1_2,2),round(r2_2,2))==(-3648.0, -0.0)
+
+def test_fe_model_ss_cant():
+    w1 = 50 # kN/m (which is the same as N/mm)
+    a1 = 2350 # mm
+    b1 = 4500 # mm
+
+    w2 = 19 # lbs/inch == 228 lbs/ft
+    a2 = 96 # inch
+    b2 = 96 # inch
+
+    E=1.
+    I=1.
+    A=1.
+    J=1.
+    nu=1.
+    model1=beams.fe_model_ss_cant(w1,b1,a1,E,I,A,J,nu)
+    model1.analyze()   
+
+    r2_1=model1.Nodes['N0'].RxnFY['Combo 1']
+    r1_1=model1.Nodes['N1'].RxnFY['Combo 1']
+    ar1_1,ar2_1=beams.beam_reactions_ss_cant(w1,b1,a1)
+    assert round(ar2_1,2)==round(r2_1,2)
+    assert round(ar1_1,2)==round(r1_1,2)
+ 
+    w2 = 19 # lbs/inch == 228 lbs/ft
+    a2 = 96 # inch
+    b2 = 96 # inch
+
+    E=1.
+    I=1.
+    A=1.
+    J=1.
+    nu=1.
+    model2=beams.fe_model_ss_cant(w2,b2,a2,E,I,A,J,nu)
+    model2.analyze()   
+
+    r2_2=model2.Nodes['N0'].RxnFY['Combo 1']
+    r1_2=model2.Nodes['N1'].RxnFY['Combo 1']
+    ar1_2,ar2_2=beams.beam_reactions_ss_cant(w2,b2,a2)
+    assert round(ar2_2,2)==round(r2_2,2)
+    assert round(ar1_2,2)==round(r1_2,2)
