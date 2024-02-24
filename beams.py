4f4037e61c78aeb835436b28e04ddd1eb28e4919
nferrinha <ferrinha.nuno@gmail.com>
2024-02-24 13:28:00 +0400

    Added spacing

--- beams.py
+++ beams.py
@@ -1,27 +1,66 @@
-import math
-from PyNite import FEModel3D
-
-def read_beam_file(filename:str)->str:
+def read_beam_file(filename:str):
     """
     Returns a string representing the text data in the file
     at 'filename'.
     """
+    file_data=[]
     with open(filename) as file:
-        file_data=file.read()
+        for line in file.readlines():
+            no_newline=line.strip()
+            file_data.append(no_newline)
     return file_data
 
-
-def str_to_int(s: str) -> int:
+def separate_data(filename:list[str])->list[list[str]]:
     """
-    Returns an integer if the string 's' represents an integer
+    The function returns a list containing the strings of a list split in new list
     """
-    return int(s)
+    split_data=[]
+    for line in filename:
+        split=line.split(", ")
+        split_data.append(split)
+    return split_data
 
 def str_to_float(s: str) -> float:
     """
     Returns an float if the string 's' represents an float
     """
     return float(s)
+
+def convert_to_numeric(filename:list[list[str]])->list[list[float]]:
+    """
+    This function converts strings inside a list into floats
+    """
+    converted_data=[]
+    for line in filename[:]:
+        numeric_data=[]
+        for sub_lines in line:
+            a=str_to_float(sub_lines.replace(",",""))
+            numeric_data.append(a)
+        converted_data.append(numeric_data)
+    return converted_data
+def get_structured_data(filename:list[list[str]])->dict:
+    """
+    This function returns a dictionary with the input beam data parcelled out.
+    """
+    beam_dict = {}
+    for line in filename:
+        beam_dict['Name']=filename[0][0]
+        beam_dict['L']=convert_to_numeric(filename[1:])[0][0]
+        beam_dict['E']=convert_to_numeric(filename[1:])[0][1]
+        beam_dict['Iz']=convert_to_numeric(filename[1:])[0][2]
+        beam_dict['Supports']=convert_to_numeric(filename[1:])[1]
+        beam_dict['Loads']=convert_to_numeric(filename[1:])[2:]
+    return beam_dict
+  
+def get_node_locations(filename:list)->dict[str,float]:
+    """
+    This function takes the list of support locations and returns a dictionary with the node names and their locations
+    """
+    node_locations={}
+    for i,support in enumerate(filename):
+        node_name=f"N{i}"
+        node_locations[node_name] = support
+    return node_locations
 
 def calc_shear_modulus (nu:float, E:float) -> float:
     """
@@ -30,53 +69,13 @@ def calc_shear_modulus (nu:float, E:float) -> float:
     G=E/(2*(1+nu))
     return G
 
-def euler_buckling_load(
-    l: float,
-    E: float,
-    I: float,
-    k: float,
-) -> float:
-    """
-    Returns the Euler critical load for the column section described by 'l', 'E', 'I' and 'k'.
-    'l': braced length of the column
-    'E': Elastic moduls
-    'I': Second moment of area
-    'k': Effective length factor
-    """
-    P_cr=math.pi**2*E*I/(k*l)**2
-    return P_cr
 
-def beam_reactions_ss_cant(
-    w:float,
-    b:float,
-    a:float,
-) -> tuple[float,float]:
+from PyNite import FEModel3D
+def build_beam(beam_data:dict,A:float=1,J:float=1,nu:float=1,rho:float=1) -> FEModel3D:
     """
-    Returms the reactions "R1" and "R2" for a simply supported beam
-    with a continuous cantilever on one end. R2 is the backspan support and R1 is the hammer support.
-    """
-    length=b+a
-    centroid_of_udl=length/2
-    equiv_point_load=w*length
-    r1=equiv_point_load*centroid_of_udl/b
-    r2=equiv_point_load-r1
-    return -r1,-r2
-
-def fe_model_ss_cant(
-    w: float,
-    b: float,
-    a: float,
-    E: float, 
-    I: float, 
-    A: float, 
-    J: float, 
-    nu: float, 
-    rho: float=1.,
-)->FEModel3D:
-    """
-    Return a Pynite.FEModel3D model of a simply supported beam
-    with a cantilever on one end. The beam is loaded with a UDL
-
+    Returns a beam finite element model for the data in 'beam_data' which is assumed to represent
+    a simply supported beam with multiple nodes with a uniform distributed load applied
+    in the direction of gravity.
     "w": The magnitude of the UDL
     "b": The length of the backspan
     "a": The length of the cantilever
@@ -87,88 +86,28 @@ def fe_model_ss_cant(
     "nu": Poisson's ratio of material
     "rho": Density of the material
     """
+    node_dict=get_node_locations(beam_data['Supports'])
+    E=beam_data['E']
+    L=beam_data['L']
+    Iz=beam_data['Iz']
+    
     model=FEModel3D()
     G=calc_shear_modulus(nu,E)
     model.add_material('default',E,G,nu,rho)
-    model.add_node("N0",0,0,0)
-    model.add_node("N1",b,0,0)
-    model.add_node("N2",b+a,0,0)
-    model.def_support("N0",True,True,True,True,True,False)
-    model.def_support("N1",False,True,False,False,False,False)
-    model.add_member("M0","N0","N2",'default',Iy=1.0,Iz=I,J=J,A=A)
-    model.add_member_dist_load("M0","Fy",w1=w,w2=w)
+    for node_number,node_coord in node_dict.items():
+        model.add_node(node_number,node_coord,0,0)
+        model.def_support(node_number,False,True,False,False,False,False)
+    model.def_support(node_number,True,True,True,True,True,False)
+    model.add_member("M0",(list((node_dict.keys()))[0]),node_number,'default',Iy=1.0,Iz=Iz,J=J,A=A)
+    
+    for idx,loads in enumerate(beam_data['Loads']):
+        model.add_member_dist_load("M0","Fy",loads[0],loads[0],loads[1],loads[2])
     return model
-
-def read_beam_file(filename:str)->str:
-    """
-    Returns a string representing the text data in the file
-    at 'filename'.
-    """
-    with open(filename) as file:
-        file_data=file.read()
-    return file_data
-
-def separate_lines(file_data):
-   """
-   Separates lines in a string containing new line characters into a list.
-
-   Args:
-       file_data: A string containing text with new line characters.
-
-   Returns:
-       A list of strings, where each element represents a line in the original data.
-   """
-   lines = file_data.splitlines()
-   return lines
-
-def extract_data(data_list, index):
-    """
-    Extracts the data list item corresponding to the index and returns it as a separate list.
-    """
-    data_item = data_list[index]
-    data_item.split()
-    return data_item.split(", ")
-
-def get_spans(beam_length:float,cant_support_loc:float):
-    """
-    This functions takes the total length of the beam and the
-    location of the cantilever support and returns the length 
-    of the backspan.
-    """
-    b=beam_length-cant_support_loc
-    a=beam_length-b
-    return a,b
-
-from PyNite import FEModel3D
-
-def build_beam(beam_data: list[str]) -> FEModel3D:
-    """
-    Returns a beam finite element model for the data in 'beam_data' which is assumed to represent
-    a simply supported beam with a cantilever at one end with a uniform distributed load applied
-    in the direction of gravity.
-    """
-    LEI = extract_data(beam_data, 0)
-    L = str_to_float(LEI[0])
-    E = str_to_float(LEI[1])
-    I = str_to_float(LEI[2])
-    supports_1_and_2 = extract_data(beam_data, 1)
-    loc_sup_1=str_to_float(supports_1_and_2[0])
-    loc_sup_2=str_to_float(supports_1_and_2[1])
-    loads=extract_data(beam_data,2)
-    udl=str_to_float(loads[0])
-    l_cant=L-loc_sup_2
-    l_backspan=L-l_cant
-    beam_data=udl,l_backspan,l_cant,E,I,1,1,1,
-    beam_model=fe_model_ss_cant(beam_data[0],beam_data[1],beam_data[2],beam_data[3],beam_data[4],beam_data[5],beam_data[6],beam_data[7])
-    return beam_model  
-
 def load_beam_model(filename:str) -> FEModel3D:
     """
     This function converts a beam data into a beam model.
     It assumes model the beam is a simply supported beam
-    with a cantilever on one end and the beam is loaded with a UDL
+    with variable number of supports and the beam is loaded with a UDL
     """
-    beam_data=read_beam_file(filename)
-    beam_lines=separate_lines(beam_data)
-    beam_model=build_beam(beam_lines)
+    beam_model=build_beam(get_structured_data(separate_data(read_beam_file(filename))))
     return beam_model
--- test_beams.py
+++ test_beams.py
@@ -1,10 +1,19 @@
 import eng_module.beams as beams
-def test_str_to_int():
-    string_1 = "43"
-    string_2 = "2000"
-      
-    assert beams.str_to_int(string_1)==43
-    assert beams.str_to_int(string_2)==2000
+import math
+def test_read_beam_file():
+    beam1_data = beams.read_beam_file('test_data/beam_1.txt')
+    assert beam1_data == [
+ '4800, 200000, 437000000',
+ '0, 3000',
+ '-10']
+
+def test_separate_data():
+    beam_1_result=[
+ '4800, 200000, 437000000',
+ '0, 3000',
+ '-10']
+    lines_data=beams.separate_data(beam_1_result)
+    assert lines_data==[['4800', '200000', '437000000'],['0', '3000'],['-10']]
 
 def test_str_to_float():
     string_1 = "43"
@@ -14,113 +23,58 @@ def test_str_to_float():
     assert beams.str_to_float(string_2)==2000.0
     assert beams.str_to_float(string_3)==324.625
 
+def test_convert_to_numeric():
+    beam1_strings=[
+ ['4800', '19200', '1000000000'],
+ ['0', '3000'],
+ ['-100', '500', '4800'],
+ ['-200', '3600', '4800']
+]
+    assert beams.convert_to_numeric(beam1_strings) ==[[4800.0, 19200.0, 1000000000.0],
+ [0.0, 3000.0],
+ [-100.0, 500.0, 4800.0],
+ [-200.0, 3600.0, 4800.0]]
+    
+def test_get_structured_data():
+    separated_data=[['Roof beam'],
+ ['4800', '19200', '1000000000'],
+ ['0', '3000', '4800'],
+ ['-100', '500', '4800'],
+ ['-200', '3600', '4800']]
+    assert beams.get_structured_data(separated_data)=={'Name': 'Roof beam',
+ 'L': 4800.0,
+ 'E': 19200.0,
+ 'Iz': 1000000000.0,
+ 'Supports': [0.0, 3000.0, 4800.0],
+ 'Loads': [[-100.0, 500.0, 4800.0], [-200.0, 3600.0, 4800.0]]}
+    
+def test_get_node_locations():
+    supports=[0.0, 3000.0, 4800.0]
+    assert beams.get_node_locations(supports)=={'N0': 0.0, 'N1': 3000.0, 'N2': 4800.0}
+
 def test_calc_shear_modulus():
-    # Material 1
-    E1 = 200000 # MPa
+    E1 = 200000
     nu1 = 0.3
+    E2 = 3645
+    nu2 = 0.2
+    assert math.isclose(beams.calc_shear_modulus(nu1, E1), 76923.077, rel_tol=1e-6)
+    assert math.isclose(beams.calc_shear_modulus(nu2, E2), 1518.75, rel_tol=1e-6)
 
-    # Material 2
-    E2 = 3645 # ksi ("ksi" == "kips per square inch"; "kips" == "kilopound" == 1000 lbs)
-    nu2 = 0.2   
-
-    assert beams.calc_shear_modulus(nu1,E1) == 76923.07692307692
-    assert beams.calc_shear_modulus(nu2,E2) == 1518.75
-
-def test_euler_buckling_load():
-    # Column 1 - Value will be in Newtons
-    l1 = 5300 # mm
-    E1 = 200000 # MPa
-    I1 = 632e6 # mm**4
-    k1 = 1.0
-
-    # Column 2 - Value will be in kips ("kips" == "kilopound" == 1000 lbs)
-    l2 = 212 # inch
-    E2 = 3645 # ksi ("ksi" == "kips per square inch")
-    I2 = 5125.4 # inch**4
-    k2 = 2.0
-
-    assert beams.euler_buckling_load(l1,E1,I1,k1)==44411463.02234584
-    assert beams.euler_buckling_load(l2,E2,I2,k2)==1025.6361727834453
-
-def test_beam_reactions_ss_cant():
-    w1 = 50 # kN/m (which is the same as N/mm)
-    a1 = 2350 # mm
-    b1 = 4500 # mm
-
-    w2 = 19 # lbs/inch == 228 lbs/ft
-    a2 = 96 # inch
-    b2 = 96 # inch
-
-    r1_1,r2_1=beams.beam_reactions_ss_cant(w1,b1,a1)
-    r1_2,r2_2=beams.beam_reactions_ss_cant(w2,b2,a2)
-   
-    assert (round(r1_1,2),round(r2_1,2))==(-260680.56,-81819.44)
-    assert (round(r1_2,2),round(r2_2,2))==(-3648.0, -0.0)
-
-def test_fe_model_ss_cant():
-    w1 = 50 # kN/m (which is the same as N/mm)
-    a1 = 2350 # mm
-    b1 = 4500 # mm
-
-    w2 = 19 # lbs/inch == 228 lbs/ft
-    a2 = 96 # inch
-    b2 = 96 # inch
-
-    E=1.
-    I=1.
-    A=1.
-    J=1.
-    nu=1.
-    model1=beams.fe_model_ss_cant(w1,b1,a1,E,I,A,J,nu)
-    model1.analyze()   
-
-    r2_1=model1.Nodes['N0'].RxnFY['Combo 1']
-    r1_1=model1.Nodes['N1'].RxnFY['Combo 1']
-    ar1_1,ar2_1=beams.beam_reactions_ss_cant(w1,b1,a1)
-    assert round(ar2_1,2)==round(r2_1,2)
-    assert round(ar1_1,2)==round(r1_1,2)
- 
-    w2 = 19 # lbs/inch == 228 lbs/ft
-    a2 = 96 # inch
-    b2 = 96 # inch
-
-    E=1.
-    I=1.
-    A=1.
-    J=1.
-    nu=1.
-    model2=beams.fe_model_ss_cant(w2,b2,a2,E,I,A,J,nu)
-    model2.analyze()   
-
-    r2_2=model2.Nodes['N0'].RxnFY['Combo 1']
-    r1_2=model2.Nodes['N1'].RxnFY['Combo 1']
-    ar1_2,ar2_2=beams.beam_reactions_ss_cant(w2,b2,a2)
-    assert round(ar2_2,2)==round(r2_2,2)
-    assert round(ar1_2,2)==round(r1_2,2)
-
-def test_read_beam_file():
-    beam1_data = beams.read_beam_file('test_data/beam_1.txt')
-    assert beam1_data == '4800, 200000, 437000000\n0, 3000\n-10'
-
-def test_separate_lines():
-    beam_1_result='4800, 200000, 437000000\n0, 3000\n-10'
-    lines_data=beams.separate_lines(beam_1_result)
-    assert lines_data==['4800, 200000, 437000000', '0, 3000', '-10']
-
-def test_extract_data():
-    beam_1_data=['4800, 200000, 437000000', '0,3000', '-10']
-    extracted_data=beams.extract_data(beam_1_data,0)
-    assert extracted_data==['4800', '200000', '437000000'] 
-
-def test_get_spans():
-    beam1_span=4000,2500
-    calculated_beam_span=beams.get_spans(beam1_span[0],beam1_span[1])
-    assert calculated_beam_span==(2500, 1500)
+def test_build_beam():
+    beam_dict = {
+        "Name": "Test Beam",
+        "L": 5000,
+        "E": 200000,
+        "Iz": 400e6,
+        "Supports": [0.0, 5000.0],
+        "Loads": [[-100, 0.0, 5000.0]],
+        "Nodes": {"N0": 0.0, "N1": 5000.0}
+    }
+    beam_model = beams.build_beam(beam_dict)
+    beam_model.analyze()
+    assert math.isclose(beam_model.Members['M0'].min_deflection("dy"), -10.172, rel_tol=1e-4)
 
 def test_load_beam_model():
-    beam_2_model=beams.load_beam_model('test_data/beam_2.txt')
-    beam_2_model.analyze()
-    support_n0=beam_2_model.Nodes['N0'].RxnFY
-    support_n1=beam_2_model.Nodes['N1'].RxnFY
-    assert round(support_n0['Combo 1'],0)==0
-    assert round(support_n1['Combo 1'],0)==3420
+    beam_model = beams.load_beam_model('test_data/beam_4-wk3.txt')
+    beam_model.analyze()
+    assert math.isclose(beam_model.Members['M0'].min_deflection("dy"),  -68.12, rel_tol=1e-4)
